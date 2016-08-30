347fc5ae34abc8523cbb9aa0bb7bcdece5721710
Jon Saxton <cem@tower21studios.com>
2016-08-30 13:18:53 -0600

    Prepared dash for adding items.

--- dash.php
+++ dash.php
@@ -21,10 +21,36 @@ else
 {
  switch ($_GET['section'])
  {
+  case 'put':
+  switch ($type)
+  {
+    //TODO change put form based on $type
+  }
+  break;
   case 'projects':
   default:
   $title="Your Projects";
   $body="<h1>{$title}</h1>\n";
+  $data=new DataBaseTable('content',true,DATACONF);
+  $q=$data->getData("pid:`= 0` uid:`= {$session->uid}`");
+  $c=0;
+  if ($q instanceof PDOStatement)
+  {
+   while ($row=$q->fetch(PDO::FETCH_ASSOC))
+   {
+    //TODO generate table rows
+    $c++;
+   }
+  }
+  
+  if ($c <= 0)
+  {
+    $body.="<div class=\"alert alert-warning\">You have '{$c}' projects! Would you like to <a href=\"./dash.php?section=put&type=project\">add one</a>?</div>\n";
+  }
+  else
+  {
+    //TODO set $body two table with above rows
+  }
  }
 }
 ?>

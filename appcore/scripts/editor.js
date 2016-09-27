82a23b54982541910546f62eb897a25eab7835c0
Jon Saxton <cem@tower21studios.com>
2016-09-27 13:43:46 -0600

    added comix script editor toolbar and button prototypes.

--- appcore/scripts/editor.js
+++ appcore/scripts/editor.js
@@ -10,10 +10,12 @@ $(document).on('change',':file',function(){
     {
         return this.each(function(){
             var val=$(this).val();
-            $(this).parent().append("<div id=\"ComiXEditor\" class=\"comix canvas\">"+ val + "</div>");
+            $(this).parent().append("<div id=\"ComiXEditor\" class=\"comix container\"></div>");
             $(this).parent().append("<div class=\"hide\"></div>");
             $(this).parent().find("div.hide").append($(this).clone());
-            $(this).parent().find("div#ComiXEditor").children().addClass('canvas-item');
+            $(this).parent().find("div#ComiXEditor").append("<div class=\"btn-toolbar\"><div class=\"btn-group\"><button class=\"btn btn-primary\">Add Asset</button></div> <div class=\"btn-group\"><button type=\"button\" class=\"btn btn-info\">Desktop</button><button type=\"button\" class=\"btn btn-info\">Tablet</button><button type=\"button\" class=\"btn btn-info active\">Phone</button></div> <div class=\"btn-group\"><div class=\"btn-group\"><button class=\"btn btn-info dropdown-toggle\" data-toggle=\"dropdown\">Add Transition <span class=\"caret\"></span></button><ul class=\"dropdown-menu\"><li>Transitions go here</li></ul></div><button class=\"btn btn-info\">Add Time Delay</button></div></div>");
+            $(this).parent().find("div#ComiXEditor").append("<div class=\"canvas phone\">"+ val +"</div>");
+            $(this).parent().find("div#ComiXEditor .canvas p").remove();
             $(this).remove();
         });
     }
@@ -1380,4 +1382,91 @@ $(document).on('change',':file',function(){
 				});
 		});
 	};
+})(jQuery);
+
+/*
+ * HTML5 Sortable jQuery Plugin
+ * http://farhadi.ir/projects/html5sortable
+ * 
+ * Copyright 2012, Ali Farhadi
+ * Released under the MIT license.
+ */
+(function($) {
+var dragging, placeholders = $();
+$.fn.sortable = function(options) {
+	var method = String(options);
+	options = $.extend({
+		connectWith: false
+	}, options);
+	return this.each(function() {
+		if (/^enable|disable|destroy$/.test(method)) {
+			var items = $(this).children($(this).data('items')).attr('draggable', method == 'enable');
+			if (method == 'destroy') {
+				items.add(this).removeData('connectWith items')
+					.off('dragstart.h5s dragend.h5s selectstart.h5s dragover.h5s dragenter.h5s drop.h5s');
+			}
+			return;
+		}
+		var isHandle, index, items = $(this).children(options.items);
+		var placeholder = $('<' + (/^ul|ol$/i.test(this.tagName) ? 'li' : /^tbody$/i.test(this.tagName) ? 'tr' : 'div')
+            + ' class="sortable-placeholder ' + options.placeholderClass + '">').html('&nbsp;');
+        items.find(options.handle).mousedown(function() {
+			isHandle = true;
+		}).mouseup(function() {
+			isHandle = false;
+		});
+		$(this).data('items', options.items)
+		placeholders = placeholders.add(placeholder);
+		if (options.connectWith) {
+			$(options.connectWith).add(this).data('connectWith', options.connectWith);
+		}
+		items.attr('draggable', 'true').on('dragstart.h5s', function(e) {
+			if (options.handle && !isHandle) {
+				return false;
+			}
+			isHandle = false;
+			var dt = e.originalEvent.dataTransfer;
+			dt.effectAllowed = 'move';
+			dt.setData('Text', 'dummy');
+			index = (dragging = $(this)).addClass('sortable-dragging').index();
+		}).on('dragend.h5s', function() {
+			if (!dragging) {
+				return;
+			}
+			dragging.removeClass('sortable-dragging').show();
+			placeholders.detach();
+			if (index != dragging.index()) {
+				dragging.parent().trigger('sortupdate', {item: dragging});
+			}
+			dragging = null;
+		}).not('a[href], img').on('selectstart.h5s', function() {
+			this.dragDrop && this.dragDrop();
+			return false;
+		}).end().add([this, placeholder]).on('dragover.h5s dragenter.h5s drop.h5s', function(e) {
+			if (!items.is(dragging) && options.connectWith !== $(dragging).parent().data('connectWith')) {
+				return true;
+			}
+			if (e.type == 'drop') {
+				e.stopPropagation();
+				placeholders.filter(':visible').after(dragging);
+				dragging.trigger('dragend.h5s');
+				return false;
+			}
+			e.preventDefault();
+			e.originalEvent.dataTransfer.dropEffect = 'move';
+			if (items.is(this)) {
+				if (options.forcePlaceholderSize) {
+					placeholder.height(dragging.outerHeight());
+				}
+				dragging.hide();
+				$(this)[placeholder.index() < $(this).index() ? 'after' : 'before'](placeholder);
+				placeholders.not(placeholder).detach();
+			} else if (!placeholders.is(this) && !$(this).children(options.items).length) {
+				placeholders.detach();
+				$(this).append(placeholder);
+			}
+			return false;
+		});
+	});
+};
 })(jQuery);

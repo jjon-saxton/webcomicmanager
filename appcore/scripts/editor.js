b5324ba817cf50adf7ebedd6c99cda5ca26460aa
Jon Saxton <cem@tower21studios.com>
2016-09-28 17:15:51 -0600

    made assets draggable.

--- appcore/scripts/editor.js
+++ appcore/scripts/editor.js
@@ -14,8 +14,9 @@ $(document).on('change',':file',function(){
             $(this).parent().append("<div class=\"hide\"></div>");
             $(this).parent().find("div.hide").append($(this).clone());
             $(this).parent().find("div#ComiXEditor").append("<div class=\"btn-toolbar\"><div class=\"btn-group\"><button type=\"button\" onclick=\"addAsset()\" class=\"btn btn-primary\">Add Asset</button></div> <div class=\"btn-group\"><button type=\"button\" id=\"desktop\" onclick=\"changeCanvas('desktop')\" class=\"btn btn-canvas btn-info\">Desktop</button><button id=\"tablet\" type=\"button\" onclick=\"changeCanvas('tablet')\" class=\"btn btn-canvas btn-info\">Tablet</button><button id=\"phone\" type=\"button\" onclick=\"changeCanvas('phone')\" class=\"btn btn-canvas btn-info active\">Phone</button></div> <div class=\"btn-group\"><div class=\"btn-group\"><button class=\"btn btn-info dropdown-toggle\" data-toggle=\"dropdown\">Add Transition <span class=\"caret\"></span></button><ul class=\"dropdown-menu\"><li>Transitions go here</li></ul></div><button class=\"btn btn-info\">Add Time Delay</button></div></div>");
+            $(this).parent().find(".canvas-asset").dr();
             $(this).parent().find("div#ComiXEditor").append("<div class=\"canvas phone\">"+ val +"</div>");
-            $(this).parent().find("div#ComiXEditor").append("<div class=\"timeline\">Timeline? Preview buttons?</div>");
+            $(this).parent().find("div#ComiXEditor").append("<div class=\"btn-toolbar\">Preview buttons?</div>");
             $(this).parent().find("div#ComiXEditor .canvas p").remove();
             $(this).remove();
             
@@ -24,7 +25,8 @@ $(document).on('change',':file',function(){
 }( jQuery ));
 
 function addAsset(){
-      $("div #ComiXEditor .canvas").append("<div class=\"canvas-asset\"><form action=\"/wcm/dash/?section=upload&type=panel\" target=\"filetarget\" method=\"post\" enctype=\"multipart/form-data\"><label for=\"file\" class=\"btn btn-primary\"><span class=\"glyphicon glyphicon-upload\"></span></label><input id=\"file\" onchange=\"iUpload(this)\" type=\"file\" name=\"art\" class=\"hide\"></form><div id=\"art\" class=\"progress no-show\"><div class=\"progress-bar\" role=\"progressbar\" aria-valuenow=\"0\" aria-valuemin=\"0\" aria-valuemax=\"100\"><span class=\"sr-only\">0%</span></div></div><iframe class=\"hide\" name=\"filetarget\"></div>");
+      $("div#ComiXEditor .canvas").append("<div class=\"canvas-asset\"><form action=\"/wcm/dash/?section=upload&type=panel\" target=\"filetarget\" method=\"post\" enctype=\"multipart/form-data\"><label for=\"file\" class=\"btn btn-primary\"><span class=\"glyphicon glyphicon-upload\"></span></label><input id=\"file\" onchange=\"iUpload(this)\" type=\"file\" name=\"art\" class=\"hide\"></form><div id=\"art\" class=\"progress no-show\"><div class=\"progress-bar\" role=\"progressbar\" aria-valuenow=\"0\" aria-valuemin=\"0\" aria-valuemax=\"100\"><span class=\"sr-only\">0%</span></div></div><iframe class=\"hide\" name=\"filetarget\"></div>");
+      $("div#ComiXEditor .canvas .canvas-asset").dr();
 }
 
 function changeCanvas(type){
@@ -33,6 +35,47 @@ function changeCanvas(type){
     $("div #ComiXEditor .canvas").addClass(type);
     $("button#"+type).addClass('active');
 }
+
+(function($){
+    $.fn.dr=function(){
+        this
+            .css('cursor','move')
+            .on('mousedown touchstart',function(e){
+                var $dr=$(this);
+                var x=$dr.offset().left-e.pageX,
+                y=$dr.offset().top-e.pageY,
+                z=$dr.css('z-index');
+                
+                if (!$.fn.dr.stack){
+                    $.fn.dr.stack=999;
+                }
+                stack=$.fn.dr.stack;
+                
+                $(window)
+                .on('mousemove.dr touchmove.dr',function(e){
+                    $dr
+                    .css({'z-index':stack,'transform':'scale(1.1)','transition':'transform .3s','bottom':'auto','right':'auto'})
+                    .offset({
+                        left:x+e.pageX,
+                        top:y+e.pageY
+                    })
+                    .find('a').one('click.dr',function(e){
+                        e.preventDefault()
+                    });
+                    
+                    e.preventDefault();
+                })
+                .one('mouseup touchend touchcancel',function(){
+                    $(this).off('mousemove.dr touchmove.dr click.dr');
+                    $dr.css({'z-index':stack,'transform':'scale(1)'})
+                    $.fn.dr.stack++;
+                });
+                e.preventDefault();
+            });
+            
+        return this;
+    }
+}( jQuery ));
 
 /*!
  *
@@ -166,8 +209,8 @@ function changeCanvas(type){
 		addParams('b','Bold','B',["b","strong"],true); // bold --> ctrl + b
 		addParams('i','Italic','I',["i","em"],true); // italic --> ctrl + i
 		addParams('u','Underline','U',["u"],true); // underline --> ctrl + u
-		addParams('ol','insertorderedlist','¾',["ol"],true); // ordered list --> ctrl + .(dot)
-		addParams('ul','insertunorderedlist','¼',["ul"],true); // unordered list --> ctrl + ,(comma)
+		addParams('ol','insertorderedlist','??',["ol"],true); // ordered list --> ctrl + .(dot)
+		addParams('ul','insertunorderedlist','??',["ul"],true); // unordered list --> ctrl + ,(comma)
 		addParams('sub','subscript','(',["sub"],true); // sub script --> ctrl + down arrow
 		addParams('sup','superscript','&',["sup"],true); // super script --> ctrl + up arrow
 		addParams('outdent','outdent','%',["blockquote"],false); // outdent --> ctrl + left arrow
@@ -866,8 +909,8 @@ function changeCanvas(type){
 				// display the style-field
 				if(status)
 				{
-					toolbar.data(mainData["d"],true); // stil seçme alanının açıldığını belirten parametre yaz 
-					mainData["f"].slideDown(100); // stil seçme alanını aç
+					toolbar.data(mainData["d"],true); // stil se??me alan??n??n a????ld??????n?? belirten parametre yaz 
+					mainData["f"].slideDown(100); // stil se??me alan??n?? a??
 					
 					// return to all datas and close the fields of external datas
 					for(var si=0; si < allData.length; si++)
@@ -1394,91 +1437,4 @@ function changeCanvas(type){
 				});
 		});
 	};
-})(jQuery);
-
-/*
- * HTML5 Sortable jQuery Plugin
- * http://farhadi.ir/projects/html5sortable
- * 
- * Copyright 2012, Ali Farhadi
- * Released under the MIT license.
- */
-(function($) {
-var dragging, placeholders = $();
-$.fn.sortable = function(options) {
-	var method = String(options);
-	options = $.extend({
-		connectWith: false
-	}, options);
-	return this.each(function() {
-		if (/^enable|disable|destroy$/.test(method)) {
-			var items = $(this).children($(this).data('items')).attr('draggable', method == 'enable');
-			if (method == 'destroy') {
-				items.add(this).removeData('connectWith items')
-					.off('dragstart.h5s dragend.h5s selectstart.h5s dragover.h5s dragenter.h5s drop.h5s');
-			}
-			return;
-		}
-		var isHandle, index, items = $(this).children(options.items);
-		var placeholder = $('<' + (/^ul|ol$/i.test(this.tagName) ? 'li' : /^tbody$/i.test(this.tagName) ? 'tr' : 'div')
-            + ' class="sortable-placeholder ' + options.placeholderClass + '">').html('&nbsp;');
-        items.find(options.handle).mousedown(function() {
-			isHandle = true;
-		}).mouseup(function() {
-			isHandle = false;
-		});
-		$(this).data('items', options.items)
-		placeholders = placeholders.add(placeholder);
-		if (options.connectWith) {
-			$(options.connectWith).add(this).data('connectWith', options.connectWith);
-		}
-		items.attr('draggable', 'true').on('dragstart.h5s', function(e) {
-			if (options.handle && !isHandle) {
-				return false;
-			}
-			isHandle = false;
-			var dt = e.originalEvent.dataTransfer;
-			dt.effectAllowed = 'move';
-			dt.setData('Text', 'dummy');
-			index = (dragging = $(this)).addClass('sortable-dragging').index();
-		}).on('dragend.h5s', function() {
-			if (!dragging) {
-				return;
-			}
-			dragging.removeClass('sortable-dragging').show();
-			placeholders.detach();
-			if (index != dragging.index()) {
-				dragging.parent().trigger('sortupdate', {item: dragging});
-			}
-			dragging = null;
-		}).not('a[href], img').on('selectstart.h5s', function() {
-			this.dragDrop && this.dragDrop();
-			return false;
-		}).end().add([this, placeholder]).on('dragover.h5s dragenter.h5s drop.h5s', function(e) {
-			if (!items.is(dragging) && options.connectWith !== $(dragging).parent().data('connectWith')) {
-				return true;
-			}
-			if (e.type == 'drop') {
-				e.stopPropagation();
-				placeholders.filter(':visible').after(dragging);
-				dragging.trigger('dragend.h5s');
-				return false;
-			}
-			e.preventDefault();
-			e.originalEvent.dataTransfer.dropEffect = 'move';
-			if (items.is(this)) {
-				if (options.forcePlaceholderSize) {
-					placeholder.height(dragging.outerHeight());
-				}
-				dragging.hide();
-				$(this)[placeholder.index() < $(this).index() ? 'after' : 'before'](placeholder);
-				placeholders.not(placeholder).detach();
-			} else if (!placeholders.is(this) && !$(this).children(options.items).length) {
-				placeholders.detach();
-				$(this).append(placeholder);
-			}
-			return false;
-		});
-	});
-};
 })(jQuery);
